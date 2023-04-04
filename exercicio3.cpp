7fdbefa57ee9438b66ace138a9cae468b511514f
JoaoGuilhermeMA <joomoreira36@gmail.com>
2023-04-04 20:04:38 -0300

    criando o exercicio 3

---
+++ exercicio3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float alturaChico = 1.50, alturaJuca = 1.10;
+    int ano = 1;
+
+    while (alturaJuca <= alturaChico)
+    {
+        alturaChico += 0.02;
+        alturaJuca += 0.03;
+        ano++;
+    }
+
+    cout << "Juca vai levar " << ano << " anos para que sua altura fique maior que a de chico" << endl;
+}

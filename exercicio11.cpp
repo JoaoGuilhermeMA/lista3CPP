a1d54852310d4d17fd67279525262c737b827024
JoaoGuilhermeMA <joomoreira36@gmail.com>
2023-04-06 16:05:24 -0300

    refatorando o codigo

--- exercicio11.cpp
+++ exercicio11.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 int main()
 {
-    int a, b, resto;
+    int A, B, resto, divisao;
 
-    cout << "Digite o valor de a e b: " << endl;
-    cin >> a >> b;
+    cout << "Digite o valor de A e B: " << endl;
+    cin >> A >> B;
 
-    resto = a - ((a/b) * b);
+    divisao = A / B;
+    resto = A - (divisao * B);
 
-    cout << "opcao 1: " << resto << endl;
-    cout << "opcao 2: " << a%b << endl;
+    cout << "O resto da divisao de A por B e: " << resto << endl;
 
 
 }

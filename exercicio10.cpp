ffc0745459ec1bc5a8adc9890670fd6d7f9e74ec
JoaoGuilhermeMA <joomoreira36@gmail.com>
2023-04-08 15:28:27 -0300

    adicionando linux

--- exercicio10.cpp
+++ exercicio10.cpp
@@ -2,15 +2,6 @@
 
 using namespace std;
 
-/*
-Faça um algoritmo que leia um conjunto de 4 valores, um de cada vez, e,
-após lidos os valores, escreva uma tabela de resultados (formatada com linhas verticais e horizontais).
-A tabela deverá conter cada valor lido em uma coluna, seu quadrado (na segunda linha) e seu cubo (na terceira linha).
-Finalizar a entrada de dados quando os 4 valores digitados forem iguais a zero.
-Se for digitado algum número negativo, solicitar que o usuário digite novamente até que o número seja positivo.
-*/
-
-
 int main()
 {
     int v1, v2, v3, v4;

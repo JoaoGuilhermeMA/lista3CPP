ca32f419e3d8cba735f9c14e8a4d47d2c73b90c2
JoaoGuilhermeMA <joomoreira36@gmail.com>
2023-04-08 17:05:22 -0300

    adicionando o exercicio 12

---
+++ exercicio12.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numero, qntdNumero, resultado;
+
+    cout << "Digite a quantidade de numero a ser digitado: " << endl;
+    cin >> qntdNumero;
+
+    for (int i = 0; i < qntdNumero; i++)
+    {
+        cout << "Digite um numero de 4 algarismos: " << endl;
+        cin >> numero;
+
+        resultado = (numero /100) + (numero%100);
+
+        if (resultado * resultado == numero)
+        {
+            cout << "Positivo" << endl;
+        }else{
+            cout << "negativo" << endl;
+        }
+
+    }
+
+
+
+
+
+}

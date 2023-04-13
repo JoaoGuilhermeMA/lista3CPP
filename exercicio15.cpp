5080bbbde13a75bd710a96369dfbd165ab2f38db
JoaoGuilhermeMA <joomoreira36@gmail.com>
2023-04-13 19:06:37 -0300

    adicionando o exercicio 15

---
+++ exercicio15.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int identidade, qntdDependentes;
+    float salarioBruto, salarioLiquido, horasTrabalhadas;
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "Escreva a identidade do funcionario: " << endl;
+        cin >> identidade;
+
+        cout << "Digite a quantidade de horas trabalhadas do funcionario <<"
+             << endl;
+        cin >> horasTrabalhadas;
+
+        do
+        {
+            cout << "Digite a quantidade de dependentes que esse funcionario tem: " << endl;
+            cin >> qntdDependentes;
+        } while (qntdDependentes < 0);
+
+        salarioBruto = (12 * horasTrabalhadas) + (40 * qntdDependentes);
+        salarioLiquido = salarioBruto * 0.865;
+        cout << "O salario bruto desse funcionario e " << salarioBruto << endl;
+        cout << "O salario liquido desse funcionario e " << salarioLiquido;
+        if (qntdDependentes > 3)
+        {
+            cout << "O funcionario tem mais de 3 dependentes, sua identidade e " << identidade;
+        }
+
+    }
+}
